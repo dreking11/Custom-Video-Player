c1f27b4c83886d6d4744f684c5220bfa2aff0fb6
Dre King <dreking11@gmail.com>
2021-09-03 16:29:24 -0400

    updated

--- script.js
+++ script.js
@@ -24,14 +24,27 @@ function updatePlayIcon() {
 
 //Update progress & timestamp 
 function updateProgress() {
-    return true;
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    //Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    //Ger secs
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 //Set video time to progress
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
-
 //stop video
 function stopVideo() {
    video.currentTime = 0;
@@ -42,7 +55,7 @@ function stopVideo() {
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('click', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
